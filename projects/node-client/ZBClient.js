2f2fe02e54823f5819084b8d8a088087f60ebf48
Random Ambersky <random@amberskynet.org>
2020-02-17 13:31:53 +0300

    add some routines

--- projects/node-client/ZBClient.js
+++ projects/node-client/ZBClient.js
@@ -1,17 +1,34 @@
 const ZB = require('zeebe-node')
 
+const _defaultConf = {
+  retry: true,
+  maxRetries: 50,
+  maxRetryTimeout: 5000
+  // onReady: () => console.log(`Connected!`),
+  // onConnectionError: () => console.log(`Disconnected!`)
+}
+
 module.exports = class ZBClient {
   constructor (gatewayAddress, conf) {
     this._gatewayAddress = gatewayAddress
-    this._conf = conf
+    this._conf = Object.assign(_defaultConf, conf)
+    this.logger = this._conf.stdout
   }
 
   connect () {
     this.zbc = new ZB.ZBClient(this._gatewayAddress, this._conf)
+    this.zbc.on('ready', () => {
+      this.logger.info('onReady is worked')
+    })
   }
 
   async topology () {
     const topology = await this.zbc.topology()
-    console.log(JSON.stringify(topology, null, 2))
+    this.logger.info('topology: %O', JSON.stringify(topology, null, 2))
+  }
+
+  async deployWorkflow (filepath) {
+    const res = await this.zbc.deployWorkflow(filepath)
+    this.logger.info('deployWorkflow: %O', res)
   }
 }
--- projects/node-client/index.js
+++ projects/node-client/index.js
@@ -1,17 +1,21 @@
+const logger = require('pino')({
+  prettyPrint: true
+})
+
 const ZBClient = require('./ZBClient')
 
 const gatewayAddress = 'localhost:26500'
 const conf = {
-  retry: true,
-  maxRetries: 50,
-  maxRetryTimeout: 5000
+  loglevel: 'INFO',
+  stdout: logger
 }
 
 const zbc = new ZBClient(gatewayAddress, conf)
 
 async function main () {
   zbc.connect()
-  await zbc.topology()
+  // await zbc.topology()
+  await zbc.deployWorkflow('./bpmn/order-process.bpmn')
 }
 
 main()
